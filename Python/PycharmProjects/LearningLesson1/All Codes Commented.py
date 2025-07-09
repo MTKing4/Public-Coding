a064c2e2a825662ab42ded617c8b2c6a5f65ad90
MTKing4 <83613147+MTKing4@users.noreply.github.com>
2025-07-09 23:30:04 +0300

    Day 17 - The Quiz project OOP

--- Python/PycharmProjects/LearningLesson1/All Codes Commented.py
+++ Python/PycharmProjects/LearningLesson1/All Codes Commented.py
@@ -2747,6 +2747,201 @@ while is_on:
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# Constructers Notes
+#NOTE 1:
+
+class User:
+    ...                                 #can also use pass
+
+user_1 = User()
+user_1.id = "001"                       #you can assign attributes to the object directly, without initializing them in the class
+user_1.username = "Angela"
+
+print(user_1.username)
+
+user_2 = User()
+
+#-----------------------------------------------------------------------------------------------------------------------
+#NOTE 2:
+
+class User:
+    def __init__(self):                                 #special function to initialize attributes, it executes everytime an object is created
+        print("new user being created...")
+
+user_1 = User()
+user_1.id = "001"                                       #creating attributes to the object manually, (without a constructor) i.e. without initializing them in the class
+user_1.username = "Angela"
+
+print(user_1.username)
+
+user_2 = User()
+
+#-----------------------------------------------------------------------------------------------------------------------
+#NOTE 3:
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id                                       # self.attribute = parameter, the convention is to have the same name for the attribute and the parameter, but it's possible for them to have different names
+        self.username = username
+        self.followers = 0                                      # provide a default value, by setting it to a value instead of a parameter, it means you don't have to pass in a parameter when creating an object (you don't have to provide the follower count at the time of the creation)
+
+
+user_1 = User("001", "Angela")                                  #creating attributes (using a constructor), this is much better with less code
+user_2 = User("002", "Jack")
+
+
+print(user_1.followers)
+
+#-----------------------------------------------------------------------------------------------------------------------
+#NOTE 3:
+
+#Adding methods to a class
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id                                       # self.attribute = parameter, the convention is to have the same name for the attribute and the parameter, but it's possible for them to have different names
+        self.username = username
+        self.followers = 0                                      # provide a default value, by setting it to a value instead of a parameter, it means you don't have to pass in a parameter when creating an object (you don't have to provide the follower count at the time of the creation)
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following =+ 1
+
+
+user_1 = User("001", "Angela")                  #creating attributes (using a constructor), this is much better with less code
+user_2 = User("002", "Jack")
+
+
+user_1.follow(user_2)
+user_2.follow(user_1)
+
+print(f"User 1 Followers: {user_1.followers}")
+print(f"User 1 Following: {user_1.following}")
+print(f"User 2 Followers: {user_2.followers}")
+print(f"User 2 Following: {user_2.following}")
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Quiz Project OOP (mostly the same between me and hers)
+#----------------------------------------------File: data.py------------------------------------------------------------
+
+question_data = [
+{"text": "A slug's blood is green.", "answer": "True"},
+{"text": "The loudest animal is the African Elephant.", "answer": "False"},
+{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+         "you are free to take it home to eat.", "answer": "True"},
+{"text": "In London, UK, if you happen to die in the House of Parliament, "
+         "you are entitled to a state funeral.", "answer": "False"},
+{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+{"text": "Google was originally called 'Backrub'.", "answer": "True"},
+{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+#----------------------------------------------File: main.py------------------------------------------------------------
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+#----------------------------------------------File: question_model-----------------------------------------------------
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+#----------------------------------------------File: quiz_brain---------------------------------------------------------
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.questions_list = q_list
+        self.score = 0
+
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)       #mind blown (this replaces if then True else then False)
+
+
+    def next_question(self):
+        question = self.questions_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {question.text} (True, False): ")
+        self.check_answer(user_answer, question.answer)
+
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f" the correct answer was: {correct_answer}")
+        print(f"Your current score is {self.score}/{self.question_number}\n")
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Notes (Differences between my main.py and hers at the begining:
+
+#mine (complicated
+
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for tick in range(0,len(question_data)-1):
+    sub_dic = question_data[tick]
+    for key in sub_dic:
+        text = sub_dic[key]
+        answer = key
+        question = Question(text,answer)
+        question_bank.append(question)
+
+print(question_bank[0].answer)
+
+#-----------------------------------
+#hers (much better)
+
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank[0].answer)
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 #inheritance
 # it is mechanism for reusing code
 
