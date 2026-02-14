fd310770cdf7ddba11b2c390b5065193d0a79719
MTKing4 <83613147+MTKing4@users.noreply.github.com>
2026-02-14 23:25:18 +0300

    Bro Code JavaScript - Asynchronous Code

--- JavaScript/LearningLesson1/All_Codes_Commented.js
+++ JavaScript/LearningLesson1/All_Codes_Commented.js
@@ -2456,3 +2456,47 @@ export function getArea(radius){
 //     <script type="module" src="index.js"></script>      <!-- add type="module" to treat the script file as a module-->
 // </body>
 // </html>
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+// Asynchronous Code
+
+
+// Synchronous = Executes line by line consecutievly in a sequential manner
+//               Code that waits for an operation to complete.
+
+// Asynchronous = ALlows multiple operations to be performed concurrently without waiting
+//                Doesn't block the execution flow and allows the program to continue
+//                (I/O operations, network requests, fetching data)
+//                Handled with: callbacks, Promises, Async/Await
+
+setTimeout(() => console.log("Task 1"), 3000)           // setTimeout() is an Asynchronous function, the rest of the code executes without waiting for it
+
+console.log("Task 2");
+console.log("Task 3");
+console.log("Task 4");
+
+
+// handling an asynchronous code with callbacks (making it synchronous)
+
+
+function func1(callback){
+    setTimeout(() => {console.log("Task 1");
+                      callback()}, 3000);    
+
+}
+
+function func2(){
+console.log("Task 2");
+console.log("Task 3");
+console.log("Task 4");
+}
+
+func1(func2);
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+
